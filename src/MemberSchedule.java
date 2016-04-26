5efa129e7e300a6e9ab6399b3d5ab177d373cdbd
Jakub Kalinowski <j.kalinowski.86@gmail.com>
2016-04-26 13:31:49 -0700

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	bin/.gitignore

---
+++ src/MemberSchedule.java
@@ -0,0 +1,8 @@
+import java.util.HashMap;
+
+public abstract class MemberSchedule {
+	
+	public abstract void addArrivals(int day, int time);
+	public abstract void addDepartures(int day, int time);
+
+}
---
+++ src/MemberShortTermSchedule.java
@@ -0,0 +1,66 @@
+import java.util.HashMap;
+
+public class MemberShortTermSchedule extends MemberSchedule{
+
+	private HashMap<Integer, Integer> shortTermArrivals;
+	private HashMap<Integer, Integer> shortTermDepartures;
+
+	private int day;
+	private int time;
+	
+	
+	/**
+	 * @param shortTermSchedule
+	 */
+	private MemberShortTermSchedule(int day, int time) {
+		
+		this.day = day;
+		this.time = time;
+	}
+
+	
+	@Override
+	public void addArrivals(int day, int time) {
+		
+		shortTermArrivals.put(day, time);
+		
+	}
+
+	@Override
+	public void addDepartures(int day, int time) {
+		
+		shortTermDepartures.put(day, time);
+		
+	}
+
+	/**
+	 * @return the shortTermDepartures
+	 */
+	public HashMap<Integer, Integer> getShortTermDepartures() {
+		return shortTermDepartures;
+	}
+
+	/**
+	 * @param shortTermDepartures the shortTermDepartures to set
+	 */
+	public void setShortTermDepartures(HashMap<Integer, Integer> shortTermDepartures) {
+		this.shortTermDepartures = shortTermDepartures;
+	}
+
+
+	/**
+	 * @return the shortTermArrivals
+	 */
+	public HashMap<Integer, Integer> getShortTermArrivals() {
+		return shortTermArrivals;
+	}
+
+
+	/**
+	 * @param shortTermArrivals the shortTermArrivals to set
+	 */
+	public void setShortTermArrivals(HashMap<Integer, Integer> shortTermArrivals) {
+		this.shortTermArrivals = shortTermArrivals;
+	}
+
+}
