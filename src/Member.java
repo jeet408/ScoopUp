e234cee93be107a2ac3d72c69ccbabc7521e2608
Dustin <dustin_tran@live.com>
2016-04-26 09:49:50 -0700

    Updated Comments for Mamber class
    organize the getters/setters

--- src/Member.java
+++ src/Member.java
@@ -28,8 +28,82 @@ public class Member extends MemberAbstraction {
 		points = 0;
 		rides = 0;
 	}
+/*****************************
+ *****************************
+ **   PUT MUTATORS HERE
+ *****************************
+ *****************************/
+	/*************************************
+	 **    SET PERSONAL INFORMATION     **
+	 *************************************/
 	
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @param email the email to set
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
 	
+	/**
+	 * @param password the password to set
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	/**
+	 * @param address the address to set
+	 */
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+/*****************************
+ *****************************
+ **   PUT ACCESSORS HERE
+ *****************************
+ *****************************/
+	/*************************************
+	 **    GET PERSONAL INFORMATION     **
+	 *************************************/
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @return the email
+	 */
+	public String getEmail() {
+		return email;
+	}
+
+
+
+	/**
+	 * @return the password
+	 */
+	public String getPassword() {
+		return password;
+	}
+
+
+	/**
+	 * @return the address
+	 */
+	public String getAddress() {
+		return address;
+	}
+
 	
 	public void setPassenger() {
 		
@@ -82,61 +156,6 @@ public class Member extends MemberAbstraction {
 		}
 	}
 
-	/**
-	 * @return the name
-	 */
-	public String getName() {
-		return name;
-	}
-
-	/**
-	 * @param name the name to set
-	 */
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	/**
-	 * @return the email
-	 */
-	public String getEmail() {
-		return email;
-	}
-
-	/**
-	 * @param email the email to set
-	 */
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	/**
-	 * @return the password
-	 */
-	public String getPassword() {
-		return password;
-	}
-
-	/**
-	 * @param password the password to set
-	 */
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	/**
-	 * @return the address
-	 */
-	public String getAddress() {
-		return address;
-	}
-
-	/**
-	 * @param address the address to set
-	 */
-	public void setAddress(String address) {
-		this.address = address;
-	}
 
 	/**
 	 * @return the hasVehicle
